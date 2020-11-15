89c4b8868b85eeec1e397c86c252e12815062e39
Albert Allagulov <goonnors@gmail.com>
2020-11-15 17:21:36 +0300

    specifying list items

--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -32,9 +32,16 @@ describe('Appointment', () => {
 
 describe('AppointmentsDayView', () => {
   let container;
+  let today;
+  let appointments;
 
   beforeEach(() => {
     container = document.createElement('div');
+    today = new Date();
+    appointments = [
+      { startsAt: today.setHours(12, 0) },
+      { startsAt: today.setHours(13, 0) }
+    ];
   });
 
   const render = component =>
@@ -48,13 +55,19 @@ describe('AppointmentsDayView', () => {
   });
 
   it('renders multiple appointments in an ol element', () => {
-    const today = new Date();
-    const appointments = [
-      {startsAt: today.setHours(12, 0) },
-      { startsAt: today.setHours(13, 0) }
-    ];
     render(<AppointmentsDayView appointments={appointments} />);
     expect(container.querySelector('ol')).not.toBeNull();
     expect(container.querySelector('ol').children).toHaveLength(2);
+  });
+
+  it('renders each appointment in an li', () => {
+    render(<AppointmentsDayView appointments={appointments} />);
+    expect(container.querySelectorAll('li')).toHaveLength(2);
+    expect(container.querySelectorAll('li')[0].textContent).toEqual(
+      '12:00'
+    );
+    expect(container.querySelectorAll('li')[1].textContent).toEqual(
+      '13:00'
+    );
   });
 });
