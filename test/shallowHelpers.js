16a1a0ac9046b096aab17febc103f6c62890f672
Albert Allagulov <aallagulov@fix.ru>
2021-04-08 12:45:01 +0300

    encapsulating render output to dry up tests

--- test/shallowHelpers.js
+++ test/shallowHelpers.js
@@ -1,4 +1,42 @@
 import React from 'react';
+import ShallowRenderer from 'react-test-renderer/shallow';
+
+const elementsMatching = (element, matcherFn) => {
+  if (matcherFn(element)) {
+    return [element];
+  }
+  return childrenOf(element).reduce(
+    (acc, child) => [
+      ...acc,
+      ...elementsMatching(child, matcherFn)
+    ],
+    []
+  );
+};
+
+export const type = typeName => element =>
+  element.type === typeName;
+
+export const id = id => element =>
+  element.props && element.props.id === id;
+
+export const className = className => element =>
+  element.props && element.props.className === className;
+
+export const click = element => element.props.onClick();
+
+export const createShallowRenderer = () => {
+  let renderer = new ShallowRenderer();
+
+  return {
+    render: component => renderer.render(component),
+    elementMatching: matcherFn =>
+      elementsMatching(renderer.getRenderOutput(), matcherFn)[0],
+    elementsMatching: matcherFn =>
+      elementsMatching(renderer.getRenderOutput(), matcherFn),
+    child: n => childrenOf(renderer.getRenderOutput())[n]
+  };
+};
 
 export const childrenOf = element => {
   if (typeof element === 'string') {
